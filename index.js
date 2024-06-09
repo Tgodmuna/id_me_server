92f024e678ce3de95a2eaf8ba8d1dc3988be5cba
ThankGod <aguthankgod@gmail.com>
2024-06-09 10:30:16 +0100

    modified:   index.js

--- index.js
+++ index.js
@@ -20,16 +20,12 @@ const server = http.createServer((req, res) => {
 	// Enable CORS
 	res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-	res.setHeader("Access-Control-Allow-Credentials", "true");
 
 	if (req.method === "OPTIONS") {
 		// Preflight request
 		res.writeHead(204, {
 			"Access-Control-Allow-Origin": "http://localhost:3000",
 			"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-			"Access-Control-Allow-Headers": "Content-Type, Authorization",
-			"Access-Control-Allow-Credentials": "true",
 		});
 		res.end();
 		return;
