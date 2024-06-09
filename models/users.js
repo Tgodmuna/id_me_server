99822fecdc401e7f6d827b732071024bf5dfe749
ThankGod <aguthankgod@gmail.com>
2024-06-09 08:41:18 +0100

    update user's schema for orchestrating data-collection form

--- models/users.js
+++ models/users.js
@@ -4,6 +4,20 @@ const userSchema = new mongoose.Schema({
 	username: { type: String, required: true, unique: true },
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
+	citizenship: { type: String, required: true },
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	dob: { type: Date, required: true },
+	address: { type: String, required: true },
+	phoneNumber: { type: String, required: true },
+	ssn: { type: String },
+	document: { type: Buffer },
+	video: { type: Buffer },
+	image: { type: Buffer },
+	iban: { type: String },
+	notifications: [{ type: String }],
+	verificationState: { type: String, enum: ["passed", "failed", "pending"], default: "pending" },
+	termsAndConditions: { type: Boolean, required: true },
 });
 
 module.exports = mongoose.model("User", userSchema);
