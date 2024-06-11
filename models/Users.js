6362f745c810fdb87bb30fe3175fc186a15c266e
ThankGod <aguthankgod@gmail.com>
2024-06-11 18:42:00 +0100

    modified user schema

--- models/Users.js
+++ models/Users.js
@@ -1,37 +1,29 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-	fullName: {
-		type: String,
-		required: true,
-		trim: true,
+const userSchema = new mongoose.Schema(
+	{
+		fullName: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		country: {
+			type: String,
+			required: true,
+		},
+		language: {
+			type: String,
+			required: true,
+		},
+		notifications: [{ type: String }],
 	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-		trim: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	country: {
-		type: String,
-		required: true,
-	},
-	language: {
-		type: String,
-		required: true,
-	},
-});
-
-const User = mongoose.models.User || mongoose.model("User", userSchema);
-
-module.exports = User;
-
-
-
-
-
-
+	{ timestamps: true }
+);
